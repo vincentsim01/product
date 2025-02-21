5283e5bbedc183eb5b186b2638c2e7005c5ca41e
Vincent Sim <129670790+vincentsim01@users.noreply.github.com>
2025-02-21 17:34:10 +0800

    extra vite

--- frontend/productdisplay/src/main.jsx
+++ frontend/productdisplay/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from "@/components/ui/provider"
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider>
+        <App />
+    </Provider>
+  </StrictMode>
+  ,
 )
